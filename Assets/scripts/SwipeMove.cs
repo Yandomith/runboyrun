c39f6a74a4d3111d85550db87c6d54577f059141
Yandomith <yandomith@gmail.com>
2023-11-05 20:00:39 +0545

    IDK how but touch works

--- Assets/scripts/SwipeMove.cs
+++ Assets/scripts/SwipeMove.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class SwipeMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 startTouchPosition, endTouchPosition;
+    private Touch touch;
+    private IEnumerator goCoroutine;
+    private bool coroutineAllowed;
+
+    private void Start(){
+        coroutineAllowed= true;
+
+    }
+    private  void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            touch= Input.GetTouch(0);
+        }
+        if (touch.phase == TouchPhase.Began)
+        {
+            startTouchPosition =touch.position;
+        }
+        if (Input.touchCount> 0 && touch.phase == TouchPhase.Ended && coroutineAllowed)
+        {
+            endTouchPosition= touch.position;
+            if ((endTouchPosition.y > startTouchPosition.y) && (Mathf.Abs(touch.deltaPosition.y)> Mathf.Abs(touch.deltaPosition.x)))
+            {
+                goCoroutine= Go(new Vector3(0f ,0f,0.25f));
+                StartCoroutine(goCoroutine);
+            }
+            else if ((endTouchPosition.y < startTouchPosition.y) && (Mathf.Abs(touch.deltaPosition.y)> Mathf.Abs(touch.deltaPosition.x)))
+            {
+                goCoroutine= Go(new Vector3(0f ,0f,-0.25f));
+                StartCoroutine(goCoroutine);
+            }
+            else if ((endTouchPosition.x < startTouchPosition.x) && (Mathf.Abs(touch.deltaPosition.x)> Mathf.Abs(touch.deltaPosition.y)))
+            {
+                goCoroutine= Go(new Vector3(-0.25f ,0f,0f));
+                StartCoroutine(goCoroutine);
+            }
+            else if ((endTouchPosition.x > startTouchPosition.x) && (Mathf.Abs(touch.deltaPosition.x)> Mathf.Abs(touch.deltaPosition.y)))
+            {
+                goCoroutine= Go(new Vector3(0.25f ,0f,0f));
+                StartCoroutine(goCoroutine);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Go(Vector3 direction)
     {
-        
+        coroutineAllowed = false;
+        for (int i = 0; i <= 2 ; i++)
+        {
+            transform.Translate(direction);
+            yield return new WaitForSeconds(0.01f);
+
+        }
+        for (int i= 0; i <=2 ;i++)
+        {
+            transform.Translate(direction);
+            yield return new WaitForSeconds(0.01f);
+        }
+        transform.Translate(direction);
+        coroutineAllowed =true;
+
     }
+    
 }
