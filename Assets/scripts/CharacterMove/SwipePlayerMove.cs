06f8e318dd20315f5832957120a820874ec0e054
Yandomith <yandomith@gmail.com>
2023-11-07 20:16:08 +0545

    haha

--- Assets/scripts/CharacterMove/SwipePlayerMove.cs
+++ Assets/scripts/CharacterMove/SwipePlayerMove.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class SwipePlayerActions : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float duckHeight = 1.0f; // Height to duck
+
     private float currentPosition = 4.0f; // Initial player position
     private float moveDistance = 5.5f; // Fixed movement distance
-    private float duckHeight = 1.0f; // Height to duck
 
     private float boundaryLeft = -1.5f; // Minimum allowed position
     private float boundaryRight = 9.5f; // Maximum allowed position
@@ -36,49 +37,29 @@ public class SwipePlayerActions : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    // Reset the flag at the beginning of each swipe
                     hasMoved = false;
                     break;
 
                 case TouchPhase.Moved:
                     if (!hasMoved)
                     {
-                        // Calculate the swipe direction based on the magnitude and angle
                         float magnitude = touch.deltaPosition.magnitude;
                         float angle = Vector2.SignedAngle(Vector2.right, touch.deltaPosition);
-
+                        
                         if (magnitude >= swipeAngleThreshold)
                         {
-                            if (angle < 0)
-                            {
-                                angle += 360; // Ensure positive angle
-                            }
-
+                            if (angle < 0) angle += 360;
+                            
                             if (angle >= minSwipeAngleRight && angle <= maxSwipeAngleRight)
-                            {
-                                // Right swipe
                                 Move(1);
-                                hasMoved = true;
-                            }
                             else if (angle >= minSwipeAngleLeft && angle <= maxSwipeAngleLeft)
-                            {
-                                // Left swipe
                                 Move(-1);
-                                hasMoved = true;
-                            }
                             else
                             {
-                                // Call the jump or duck method from the separate script
                                 if (angle >= jumpAndDuckScript.minSwipeAngleJump && angle <= jumpAndDuckScript.maxSwipeAngleJump)
-                                {
                                     jumpAndDuckScript.Jump();
-                                    hasMoved = true;
-                                }
                                 else if (angle >= jumpAndDuckScript.minSwipeAngleDuck && angle <= jumpAndDuckScript.maxSwipeAngleDuck)
-                                {
                                     jumpAndDuckScript.Duck();
-                                    hasMoved = true;
-                                }
                             }
                         }
                     }
@@ -89,13 +70,9 @@ public class SwipePlayerActions : MonoBehaviour
 
     void Move(float direction)
     {
-        // Calculate the new position based on the direction and fixed distance
         float newPosition = currentPosition + (direction * moveDistance);
-
-        // Ensure the player stays within the boundaries
         newPosition = Mathf.Clamp(newPosition, boundaryLeft, boundaryRight);
 
-        // Update the player's position and set the flag to indicate movement
         transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
         currentPosition = newPosition;
         hasMoved = true;
