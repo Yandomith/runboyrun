954e7e1c4cbaccf836a920aa8d175d626a04e95d
Yandomith <yandomith@gmail.com>
2023-11-07 13:50:50 +0545

    huga chaka huga huga

--- Assets/scripts/CharacterMove/Jumping.cs
+++ Assets/scripts/CharacterMove/Jumping.cs
@@ -5,6 +5,7 @@ public class Jumping : MonoBehaviour
     public float jumpHeight = 2.0f; // Height to jump
     private float initialPositionY;
     private bool isJumping = false;
+    private bool isReturning = false;
 
     void Start()
     {
@@ -17,39 +18,57 @@ public class Jumping : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Ended && touch.deltaPosition.y > 0 && !isJumping)
+            if (touch.phase == TouchPhase.Ended && touch.deltaPosition.y > 0 && !isJumping && !isReturning)
             {
                 // Swipe up (jump)
                 Jump();
             }
+        }
+
+        if (isJumping)
+        {
+            PerformJump();
+        }
+
+        if (isReturning)
+        {
+            PerformReturn();
         }
     }
 
     void Jump()
     {
         isJumping = true;
+    }
+
+    void PerformJump()
+    {
         float targetY = initialPositionY + jumpHeight;
         float currentY = transform.position.y;
+        float step = Time.deltaTime * 6.0f; // Faster jump speed
 
-        while (currentY < targetY)
+        currentY = Mathf.MoveTowards(currentY, targetY, step);
+        transform.position = new Vector3(transform.position.x, currentY, transform.position.z);
+
+        if (currentY == targetY)
         {
-            currentY += Time.deltaTime * jumpHeight; // Adjust the jump speed here
-            transform.position = new Vector3(transform.position.x, currentY, transform.position.z);
+            isJumping = false;
+            isReturning = true;
         }
+    }
 
-        // Ensure the player reaches the exact target height
-        transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
+    void PerformReturn()
+    {
+        float targetY = initialPositionY;
+        float currentY = transform.position.y;
+        float step = Time.deltaTime * 6.0f; // Faster return speed
 
-        // Return to the initial position
-        while (currentY > initialPositionY)
+        currentY = Mathf.MoveTowards(currentY, targetY, step);
+        transform.position = new Vector3(transform.position.x, currentY, transform.position.z);
+
+        if (currentY == targetY)
         {
-            currentY -= Time.deltaTime * jumpHeight; // Adjust the return speed here
-            transform.position = new Vector3(transform.position.x, currentY, transform.position.z);
+            isReturning = false;
         }
-
-        // Ensure the player returns to the initial position
-        transform.position = new Vector3(transform.position.x, initialPositionY, transform.position.z);
-
-        isJumping = false;
     }
 }
--- Assets/scripts/CharacterMove/SwipePlayerMove.cs
+++ Assets/scripts/CharacterMove/SwipePlayerMove.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 
-public class SwipePlayerMove : MonoBehaviour
+public class SwipePlayerActions : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private float currentPosition = 4.0f; // Initial player position
     private float moveDistance = 5.5f; // Fixed movement distance
+    private float jumpHeight = 2.0f; // Height to jump
+    private float duckHeight = 1.0f; // Height to duck
 
     private float boundaryLeft = -1.5f; // Minimum allowed position
     private float boundaryRight = 9.5f; // Maximum allowed position
     private bool hasMoved = false; // Flag to track if the player has moved in the current swipe
+
+    public float swipeAngleThreshold = 30.0f; // Swipe angle threshold in degrees
+
+    public float minSwipeAngle = 45.0f; // Minimum accepted swipe angle (left) 
+    public float maxSwipeAngle = 135.0f; // Maximum accepted swipe angle (right)
 
     void Update()
     {
@@ -26,22 +33,75 @@ public class SwipePlayerMove : MonoBehaviour
                 case TouchPhase.Moved:
                     if (!hasMoved)
                     {
-                        // Calculate the swipe direction
-                        float direction = Mathf.Sign(touch.deltaPosition.x);
+                        // Calculate the swipe direction based on the magnitude and angle
+                        float magnitude = touch.deltaPosition.magnitude;
+                        float angle = Vector2.Angle(Vector2.right, touch.deltaPosition);
 
-                        // Calculate the new position based on the direction and fixed distance
-                        float newPosition = currentPosition + (direction * moveDistance);
+                        if (magnitude >= touch.deltaPosition.x && magnitude >= touch.deltaPosition.y)
+                        {
+                            // Check the angle to determine if it's within the accepted range
+                            if (angle >= minSwipeAngle && angle <= maxSwipeAngle)
+                            {
+                                // Swipe within the desired range
+                                if (touch.deltaPosition.x > 0)
+                                {
+                                    // Swipe right
+                                    Move(1);
+                                }
+                                else
+                                {
+                                    // Swipe left
+                                    Move(-1);
+                                }
 
-                        // Ensure the player stays within the boundaries
-                        newPosition = Mathf.Clamp(newPosition, boundaryLeft, boundaryRight);
+                                hasMoved = true;
+                            }
+                        }
+                    }
+                    break;
 
-                        // Update the player's position and set the flag to indicate movement
-                        transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
-                        currentPosition = newPosition;
-                        hasMoved = true;
+                case TouchPhase.Ended:
+                    // Detect swipe gestures based on vertical swipe distance
+                    if (touch.deltaPosition.y > 0 && touch.deltaPosition.y > Mathf.Abs(touch.deltaPosition.x))
+                    {
+                        // Swipe up (jump)
+                        Jump();
+                    }
+                    else if (touch.deltaPosition.y < 0 && Mathf.Abs(touch.deltaPosition.y) > Mathf.Abs(touch.deltaPosition.x))
+                    {
+                        // Swipe down (duck)
+                        Duck();
                     }
                     break;
             }
         }
+    }
+
+    void Jump()
+    {
+        // Move the player upwards for jumping
+        Vector3 jumpTarget = transform.position + Vector3.up * jumpHeight;
+        transform.position = new Vector3(transform.position.x, jumpTarget.y, transform.position.z);
+    }
+
+    void Duck()
+    {
+        // Move the player downwards for ducking
+        Vector3 duckTarget = transform.position + Vector3.down * duckHeight;
+        transform.position = new Vector3(transform.position.x, duckTarget.y, transform.position.z);
+    }
+
+    void Move(float direction)
+    {
+        // Calculate the new position based on the direction and fixed distance
+        float newPosition = currentPosition + (direction * moveDistance);
+
+        // Ensure the player stays within the boundaries
+        newPosition = Mathf.Clamp(newPosition, boundaryLeft, boundaryRight);
+
+        // Update the player's position and set the flag to indicate movement
+        transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
+        currentPosition = newPosition;
+        hasMoved = true;
     }
 }
