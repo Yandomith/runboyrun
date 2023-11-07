5cf37d01abe4eff682773b0248b21b35eaf6c7f9
Yandomith <yandomith@gmail.com>
2023-11-07 11:57:18 +0545

    new camera follow script

--- Assets/scripts/CharacterMove/CameraFollow.cs
+++ Assets/scripts/CharacterMove/CameraFollow.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraFollowHead : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform headTransform;    // The head's transform
+    public float positionSmoothing = 5f; // Smoothing factor for position
+    public float rotationSmoothing = 5f; // Smoothing factor for rotation
+
+    private Vector3 initialOffset;
+    private Quaternion initialRotation;
+
+    private void Start()
     {
-        
+        // Calculate the initial offset and rotation relative to the head
+        initialOffset = transform.position - headTransform.position;
+        initialRotation = Quaternion.Inverse(headTransform.rotation) * transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Smoothly update the camera's position and rotation based on the head
+        Vector3 targetPosition = headTransform.position + initialOffset;
+        Quaternion targetRotation = headTransform.rotation * initialRotation;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, positionSmoothing * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothing * Time.deltaTime);
     }
 }
+
