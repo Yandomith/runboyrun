292c68d49ad5ce342245013c9703e846ef94b3b0
Yandomith <yandomith@gmail.com>
2023-11-16 15:08:08 +0545

    camera done

--- Assets/scripts/CharacterMove/CameraFollow.cs
+++ Assets/scripts/CharacterMove/CameraFollow.cs
@@ -3,27 +3,25 @@ using UnityEngine;
 public class CameraFollowHead : MonoBehaviour
 {
     public Transform headTransform;    // The head's transform
-    public float positionSmoothing = 10f; // Smoothing factor for position
     public float rotationSmoothing = 1f; // Smoothing factor for rotation
 
-    private Vector3 initialOffset;
     private Quaternion initialRotation;
 
     private void Start()
     {
-        // Calculate the initial offset and rotation relative to the head
-        initialOffset = transform.position - headTransform.position;
+        // Calculate the initial rotation relative to the head
         initialRotation = Quaternion.Inverse(headTransform.rotation) * transform.rotation;
     }
 
     private void Update()
     {
-        // Smoothly update the camera's position and rotation based on the head
-        Vector3 targetPosition = headTransform.position + initialOffset;
+        // Smoothly update the camera's rotation based on the head
         Quaternion targetRotation = headTransform.rotation * initialRotation;
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, positionSmoothing * Time.deltaTime);
+        // Smoothly update the camera's rotation
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothing * Time.deltaTime);
+        
+        // Set the camera's position to match the parent object
+        transform.position = headTransform.position;
     }
 }
-
