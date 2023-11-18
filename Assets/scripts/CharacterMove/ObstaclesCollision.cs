f05560ee65749b01e8b108301e71dd3661767354
Yandomith <yandomith@gmail.com>
2023-11-18 20:53:39 +0545

    hehe idk

--- Assets/scripts/CharacterMove/ObstaclesCollision.cs
+++ Assets/scripts/CharacterMove/ObstaclesCollision.cs
@@ -3,9 +3,23 @@ using PlayerMove;
 
 public class ObstaclesCollision : MonoBehaviour {
     public GameObject Player;
+    public GameObject Char;
 
-    void OnTriggerEnter(Collider other) {
+
+    void OnTriggerEnter (Collider other) 
+    {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        Debug.Log("Hit");
         Player.GetComponent<PlayerMove.SwipePlayerActions>().enabled = false;
+        Player.GetComponent<JumpAndDuckScript>().enabled = false;
+        
+        Char.GetComponent<Animator>().Play("highitFall");
+        Debug.Log("Animation changed");
+
+        if((Player.GetComponent<PlayerMove.SwipePlayerActions>().enabled == false) && (Player.GetComponent<JumpAndDuckScript>().enabled == false))
+        {
+            Char.GetComponent<Animator>().Play("highitFall");
+            Debug.Log("Forced Animation changed ");
+        }
     }
 }
