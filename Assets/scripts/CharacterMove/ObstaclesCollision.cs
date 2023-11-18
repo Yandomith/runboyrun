c5aeabf67b3790d575eb5213222b8408cbc61c8e
Yandomith <yandomith@gmail.com>
2023-11-18 19:49:58 +0545

    hehe updated script

--- Assets/scripts/CharacterMove/ObstaclesCollision.cs
+++ Assets/scripts/CharacterMove/ObstaclesCollision.cs
@@ -1,14 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstaclesCollision : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject player;
 
     void OnTriggerEnter(Collider other)
-    {   
-        this.gameObject.GetComponent<BoxCollider>().enabled=false;
-        Player.GetComponent<SwipePlayerMove>().enabled= false;
+    {
+        // Check if the colliding object is the player
+        if (other.CompareTag("Player"))
+        {
+            // Disable the collider of the current GameObject
+            GetComponent<Collider>().enabled = false;
+
+            // Disable the SwipePlayerMove script on the player GameObject
+            player.GetComponent<UnityEngine.SwipePlayerMove>().enabled = false;
+        }
     }
 }
--- Assets/scripts/CharacterMove/SwipePlayerMove.cs
+++ Assets/scripts/CharacterMove/SwipePlayerMove.cs
@@ -1,73 +1,75 @@
 using UnityEngine;
 
-public class SwipePlayerActions : MonoBehaviour
-{
-    public float moveSpeed = 10f;
-    private float currentPosition = 4.0f; // Initial player position
-    private float moveDistance = 5.5f; // Fixed movement distance
-
-    private float boundaryLeft = -1.5f; // Minimum allowed position
-    private float boundaryRight = 9.5f; // Maximum allowed position
-    private bool hasMoved = false; // Flag to track if the player has moved in the current swipe
-
-    public float swipeAngleThreshold = 10.0f; // Reduced swipe angle threshold in degrees
-
-    public float minSwipeAngleRight = 330.0f; // Minimum accepted swipe angle for right swipe
-    public float maxSwipeAngleRight = 360.0f; // Maximum accepted swipe angle for right swipe
-    public float minSwipeAngleLeft = 150.0f; // Minimum accepted swipe angle for left swipe
-    public float maxSwipeAngleLeft = 210.0f; // Maximum accepted swipe angle for left swipe
-
-    // Reference to the jumping and ducking script
-    public JumpAndDuckScript jumpAndDuckScript;
-
-    void Start()
+namespace PlayerMove{
+    public class SwipePlayerActions : MonoBehaviour
     {
-        // Initialize the reference to the jumping and ducking script
-        jumpAndDuckScript = GetComponent<JumpAndDuckScript>();
-    }
+        public float moveSpeed = 10f;
+        private float currentPosition = 4.0f; // Initial player position
+        private float moveDistance = 5.5f; // Fixed movement distance
 
-    void Update()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        private float boundaryLeft = -1.5f; // Minimum allowed position
+        private float boundaryRight = 9.5f; // Maximum allowed position
+        private bool hasMoved = false; // Flag to track if the player has moved in the current swipe
 
-        if (Input.touchCount > 0)
+        public float swipeAngleThreshold = 10.0f; // Reduced swipe angle threshold in degrees
+
+        public float minSwipeAngleRight = 330.0f; // Minimum accepted swipe angle for right swipe
+        public float maxSwipeAngleRight = 360.0f; // Maximum accepted swipe angle for right swipe
+        public float minSwipeAngleLeft = 150.0f; // Minimum accepted swipe angle for left swipe
+        public float maxSwipeAngleLeft = 210.0f; // Maximum accepted swipe angle for left swipe
+
+        // Reference to the jumping and ducking script
+        public JumpAndDuckScript jumpAndDuckScript;
+
+        void Start()
         {
-            Touch touch = Input.GetTouch(0);
+            // Initialize the reference to the jumping and ducking script
+            jumpAndDuckScript = GetComponent<JumpAndDuckScript>();
+        }
 
-            switch (touch.phase)
+        void Update()
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+
+            if (Input.touchCount > 0)
             {
-                case TouchPhase.Began:
-                    hasMoved = false;
-                    break;
+                Touch touch = Input.GetTouch(0);
 
-                case TouchPhase.Moved:
-                    if (!hasMoved)
-                    {
-                        float magnitude = touch.deltaPosition.magnitude;
-                        float angle = Vector2.SignedAngle(Vector2.right, touch.deltaPosition);
-                        
-                        if (magnitude >= swipeAngleThreshold)
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        hasMoved = false;
+                        break;
+
+                    case TouchPhase.Moved:
+                        if (!hasMoved)
                         {
-                            if (angle < 0) angle += 360;
+                            float magnitude = touch.deltaPosition.magnitude;
+                            float angle = Vector2.SignedAngle(Vector2.right, touch.deltaPosition);
                             
-                            if (angle >= minSwipeAngleRight && angle <= maxSwipeAngleRight)
-                                Move(1);
-                            else if (angle >= minSwipeAngleLeft && angle <= maxSwipeAngleLeft)
-                                Move(-1);
+                            if (magnitude >= swipeAngleThreshold)
+                            {
+                                if (angle < 0) angle += 360;
+                                
+                                if (angle >= minSwipeAngleRight && angle <= maxSwipeAngleRight)
+                                    Move(1);
+                                else if (angle >= minSwipeAngleLeft && angle <= maxSwipeAngleLeft)
+                                    Move(-1);
+                            }
                         }
-                    }
-                    break;
+                        break;
+                }
             }
         }
-    }
 
-    void Move(float direction)
-    {
-        float newPosition = currentPosition + (direction * moveDistance);
-        newPosition = Mathf.Clamp(newPosition, boundaryLeft, boundaryRight);
+        void Move(float direction)
+        {
+            float newPosition = currentPosition + (direction * moveDistance);
+            newPosition = Mathf.Clamp(newPosition, boundaryLeft, boundaryRight);
 
-        transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
-        currentPosition = newPosition;
-        hasMoved = true;
+            transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
+            currentPosition = newPosition;
+            hasMoved = true;
+        }
     }
 }
