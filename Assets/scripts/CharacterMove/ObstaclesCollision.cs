67abe701879dc242697cd4c00d54094ece79be3f
Yandomith <yandomith@gmail.com>
2023-11-18 19:53:34 +0545

    hehe obstacles collision

--- Assets/scripts/CharacterMove/ObstaclesCollision.cs
+++ Assets/scripts/CharacterMove/ObstaclesCollision.cs
@@ -1,19 +1,11 @@
 using UnityEngine;
+using PlayerMove;
 
-public class ObstaclesCollision : MonoBehaviour
-{
-    public GameObject player;
+public class ObstaclesCollision : MonoBehaviour {
+    public GameObject Player;
 
-    void OnTriggerEnter(Collider other)
-    {
-        // Check if the colliding object is the player
-        if (other.CompareTag("Player"))
-        {
-            // Disable the collider of the current GameObject
-            GetComponent<Collider>().enabled = false;
-
-            // Disable the SwipePlayerMove script on the player GameObject
-            player.GetComponent<UnityEngine.SwipePlayerMove>().enabled = false;
-        }
+    void OnTriggerEnter(Collider other) {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        Player.GetComponent<PlayerMove.SwipePlayerActions>().enabled = false;
     }
 }
