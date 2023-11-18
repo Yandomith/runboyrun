40fe497296088e605ee81434299753fe078daa90
Yandomith <yandomith@gmail.com>
2023-11-18 14:45:15 +0545

    obstacles

---
+++ Assets/scripts/ObstaclesCollision.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesCollision : MonoBehaviour
+{
+    public GameObject thePlayer;
+
+    void OnTriggerEnter(Collider other)
+    {   
+        this.gameObject.GetComponent<BoxCollider>().enabled=false;
+        thePlayer.GetComponent<SwipePlayerMove>().enabled= false;
+
+
+    }
+}
