0912e2ca483f9213178c194bb32d1e813491e898
Yandomith <yandomith@gmail.com>
2023-11-19 22:18:13 +0545

    Idea fucked

--- Assets/scripts/Environment/EnvironmentMove.cs
+++ Assets/scripts/Environment/EnvironmentMove.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class EnvironmentMove : MonoBehaviour
 {
 
+     // public GameObject LevelControls;
      public float moveSpeed = 8f;
 
        void Update()
     {
          transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+     //     if (LevelControls.GetComponent<IsOut>().enabled == false)
+     //     {
+     //           this.gameObject.GetComponent<EnvironmentMove>().enabled = false;
+     //     }
        
     }
 }
