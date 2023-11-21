4c5a3e59db097bef2042780ae0708c3032ba606a
Yandomith <yandomith@gmail.com>
2023-11-21 13:57:29 +0545

    Coin Sound

--- Assets/scripts/Collectables/CoinCollect.cs
+++ Assets/scripts/Collectables/CoinCollect.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CoinCollect : MonoBehaviour
 {
+
+    public AudioSource CoinSFX;
    void OnTriggerEnter(Collider other)
    {
+        CoinSFX.Play();
         this.gameObject.SetActive(false);
    }
 }
