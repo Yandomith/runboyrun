fb0694801b62cf5a9252071a68f97df67196770b
Yandomith <yandomith@gmail.com>
2023-11-21 13:03:46 +0545

    Coin Collected

--- Assets/scripts/Collectables/CoinCollect.cs
+++ Assets/scripts/Collectables/CoinCollect.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class CoinCollect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void OnTriggerEnter(Collider other)
+   {
+        this.gameObject.SetActive(false);
+   }
 }
